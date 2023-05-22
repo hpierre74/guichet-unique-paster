0e9ad28541e50b0c8fa6a52a3a9bbc7b07a74cd7
hpierre74 <pierrehuyghe.pro@gmail.com>
2023-05-22 18:19:59 +0200

    refactor(formatter): clean bad placeholder

--- src/pages/Content/utils/formatter.ts
+++ src/pages/Content/utils/formatter.ts
@@ -1,39 +1,10 @@
 import { set } from 'immutable-set';
 
-export type GuichetFormDa = {
-  [key: string]: string | boolean | null;
-  'common.nomDossier': string;
-  'entrepreneur-description.prenoms': string;
-  'entrepreneur-description.prenoms1': string;
-  'entrepreneur-description.nom': string;
-  'entrepreneur-description.titre': string;
-  'entrepreneur-description.nomUsage': string;
-  'entrepreneur-description.pseudonyme': string;
-  'entrepreneur-description.dateDeNaissance': string;
-  'entrepreneur-description.paysNaissance': string;
-  'entrepreneur-description.lieuDeNaissance': string;
-  'entrepreneur-description.indicateurDeNonSedentarite': boolean;
-  'entrepreneur-adresse.codePays': string;
-  'entrepreneur-adresse.voie': '76 RUE Félix Brun 69007 Lyon';
-  'entrepreneur-adresse.complementLocalisation': string;
-  'entrepreneur-contact.mail': string;
-  'entrepreneur-contact.phoneCode': string;
-  'entrepreneur-contact.telephone': string;
-  'entrepreneur-volet-social.numeroSecu': string;
-  'entrepreneur-volet-social.indicateurActiviteAnterieure': boolean;
-  'entrepreneur-volet-social.indicateurActiviteAnterieure_1': boolean;
-  'entrepreneur-volet-social.organismeAssuranceMaladieActuelle': string;
-  'entrepreneur-volet-social.demandeAcre': boolean;
-  'entrepreneur-volet-social.demandeAcre_1': boolean;
-  'entrepreneur-volet-social.activiteSimultanee': boolean;
-  'entrepreneur-volet-social.activiteSimultanee_1': boolean;
-  'entrepreneur-volet-social.affiliationPamBiologiste': boolean;
-  'entrepreneur-volet-social.affiliationPamBiologiste_1': boolean;
-  'entrepreneur-volet-social.affiliationPamPharmacien': boolean;
-  'entrepreneur-volet-social.affiliationPamPharmacien_1': boolean;
+export type GuichetFormMapping = {
+  [key: string]: string;
 };
 
-export const formatToGuichetUnique = (data: GuichetFormDa) => {
+export const formatToGuichetUnique = (data: any) => {
   let formattedData = {};
 
   Object.keys(data).forEach((key) => {
